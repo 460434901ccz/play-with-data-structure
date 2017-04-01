3cf8c376b59cdee789460e621a88ac911dc8c599
ccz <460434901ccz@gmail.com>
2017-04-01 17:06:02 +0800

    增加判断是否为空的方法

--- src/main/java/ccz/practise/playwithdatastructure/stack/Stack.java
+++ src/main/java/ccz/practise/playwithdatastructure/stack/Stack.java
@@ -4,6 +4,7 @@ package ccz.practise.playwithdatastructure.stack;
  * Created by visionsky on 2017/4/1.
  */
 public interface Stack<T> {
+    boolean isEmpty();
     T pop();
     void push(T t);
     T getTop();
